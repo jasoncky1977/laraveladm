7e2347a9b93464db76e0b3656a1f1789e0c058af
Kam Yew Choo <jason.choo@sourceforte.com>
2024-03-01 10:31:45 +0800

    third commit

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,6 @@
 <?php
+Route::post('register', 'Api\\AuthController@register');
+Route::post('login', 'Api\\AuthController@login');
 
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
     // Clients
